2f203735a969a6efad6a167fa742bfb2996b5e1e
Yahaya Kehinde <y.hkehinde@yahoo.com>
2020-04-26 10:17:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener ('change', changeBckgd)
+checkbox.addEventListener ('change', changeColor)
+
+
+function changeBckgd (){
+  document.body.classList.toggle('change-color');
+ 
+  
+  
+}
+
+
